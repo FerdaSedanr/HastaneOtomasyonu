f997f826ef88bdae3fc94714b5bcbcab49785623
FerdaSedanr <ferdasedanur.mercimek@gmail.com>
2023-10-26 23:39:11 +0300

    HastaneOtomasyonu
    C# FormEntity Framework ile Katmanlı Mimariyle Oluşturulmuş hastane otomasyonu.

---
+++ Hastane Proje  Son/DataAccessLayer/Baglanti.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+
+
+
+namespace DataAccessLayer
+{
+    public class Baglanti
+    {
+        public static SqlConnection bgl = new SqlConnection("Data Source=LAPTOP-U226G4BV;Initial Catalog=DBHastaneOtomasyon;Integrated Security=True");
+    }
+}
